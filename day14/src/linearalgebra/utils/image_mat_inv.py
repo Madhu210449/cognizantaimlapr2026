b7928afdea6b531c9030f9128f649c1930737c9f
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-12 15:17:34 +0530

    updated

--- day14/src/linearalgebra/utils/image_mat_inv.py
+++ day14/src/linearalgebra/utils/image_mat_inv.py
@@ -1,7 +1,6 @@
 #inverse image matrix using logo.webp
 import numpy as np
 import cv2
-from sympy import im
 from linearalgebra.configurations.conf import Config
 def show_image_matrix(image_path):
     #read the image using opencv    
@@ -16,18 +15,18 @@ def show_image_matrix(image_path):
     cv2.destroyAllWindows()
 
 def inverse_image(image_path):
-    img = cv2.imread(image_path)
+    img=cv2.imread(image_path)
 
-    print("Original Image Matrix:")
-    print(img.shape)   # (height, width, 3)
     # Resize image
     img = cv2.resize(img, (256, 256))
 
     # Convert color image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    cv2.imshow("Original Image", gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     # Convert to float
-    img_matrix = img.astype(np.float64)
+    img_matrix = gray.astype(np.float64)
 
     # Add small value to diagonal to avoid singular matrix
     img_matrix = img_matrix + np.eye(256) * 0.001
