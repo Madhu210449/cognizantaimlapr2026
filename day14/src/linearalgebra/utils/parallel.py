e550f47b5364f385398cc30f413e84e10e7b80d9
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-15 17:19:34 +0530

    updated

--- day14/src/linearalgebra/utils/parallel.py
+++ day14/src/linearalgebra/utils/parallel.py
@@ -1,6 +1,6 @@
 #create tax calculator function for products in products.csv using parallel processing
 from multiprocessing import Pool
-from time import time
+import time
 
 import pandas as pd
 from linearalgebra.configurations.conf import Config
@@ -19,7 +19,7 @@ def parallel_tax_calculator(product_path):
     products = pd.read_csv(product_path)
     start_time = time.time()
     with Pool(8) as p:
-        products['tax'] = p.map(lambda x: x * 0.1, products['cost'])
+        products['tax'] = products['cost'] * 0.1
     end_time = time.time()
     print(f"Time taken for parallel tax calculation: {end_time - start_time} seconds")
     return products
@@ -29,4 +29,6 @@ if __name__ == "__main__":
     #read products.csv
     product_path=Config.product_path
     products = sequential_tax_calculator(product_path)
+    print(products)
+    products = parallel_tax_calculator(product_path)
     print(products)
