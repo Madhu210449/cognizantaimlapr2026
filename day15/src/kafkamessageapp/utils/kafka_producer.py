261fb687d8ed82f09edda9f18ec6232b6a3d2477
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-18 10:50:15 +0530

    updated

--- day15/src/kafkamessageapp/utils/kafka_producer.py
+++ day15/src/kafkamessageapp/utils/kafka_producer.py
@@ -1,13 +1,13 @@
 #publish kafka message to aiven kafka cluster topic name comedy-movies
 from pathlib import Path
 from confluent_kafka import Producer
-from kafkamessageapp.configurations.conf import Config
+from kafkamessageapp.configurations.conf import KafkaConfig
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 def kafka_config():
     conf = {
-        'bootstrap.servers': Config.KAFKA_BOOTSTRAP_SERVERS,
-        'security.protocol': Config.KAFKA_SECURITY_PROTOCOL,
+        'bootstrap.servers': KafkaConfig.BOOTSTRAP_SERVERS,
+        'security.protocol': KafkaConfig.SECURITY_PROTOCOL,
         'ssl.ca.location': str(BASE_DIR / "ca.pem"),
         'ssl.certificate.location': str(BASE_DIR / "service.cert"),
         'ssl.key.location': str(BASE_DIR / "service.key"),
